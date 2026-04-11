1ad3373a22d6b81112fbd2fe0aa75e337921a26c
kyra-brown <kbrown3@bowdoin.edu>
2026-04-10 22:00:43 -0400

    index page functionality and styling.

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const cards = document.querySelectorAll('.green-flashcard');
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        const topicName = card.querySelector('h2').innerText;
+        alert('You selected: ${topicName}')
+    })
+})
