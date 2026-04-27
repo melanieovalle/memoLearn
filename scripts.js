d5ddbd3dc0957d201bbec5b6589144ee4f9f82e2
aj-lopes <anavexp@gmail.com>
2026-04-26 20:29:25 -0400

    local storage

--- scripts.js
+++ scripts.js
@@ -6,4 +6,67 @@ cards.forEach(card => {
     })
 })
 
+// functions for signup, login and local storage set up. 
+
+function handleSignup() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+
+    emailError.classList.remove('visible');
+    passwordError.classList.remove('visible');
+    document.getElementById('email').classList.remove('error');
+    document.getElementById('password').classList.remove('error');
+
+    let valid = true;
+
+    if (!password || password.length < 6) {
+        passwordError.classList.add('visible');
+        document.getElementById('password').classList.add('error');
+        valid = false;
+    }
+
+    if (valid) {
+        // store in localStorage
+        localStorage.setItem('userEmail', email);
+        localStorage.setItem('userPassword', password);
+        alert('Signup successful. Login in');
+        window.location.href = 'index.html';
+    }
+}
+
+
+function handleLogin() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+
+    emailError.classList.remove('visible');
+    passwordError.classList.remove('visible');
+    document.getElementById('email').classList.remove('error');
+    document.getElementById('password').classList.remove('error');
+
+    let valid = true;
+
+    if (!password || password.length < 6) {
+        passwordError.classList.add('visible');
+        document.getElementById('password').classList.add('error');
+        valid = false;
+    }
+
+    if (valid) {
+        const storedEmail = localStorage.getItem('userEmail');
+        const storedPassword = localStorage.getItem('userPassword');
+
+        if (email === storedEmail && password === storedPassword) {
+            alert('Login successful');
+            window.location.href = 'index.html';
+        } else {
+            alert('Invalid email or password.');
+        }
+    }
+}
+
 
